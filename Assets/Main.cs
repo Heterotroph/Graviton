588cec9179ffe3b61a7394d6f2889e2180198a7b
Heterotroph <r.akhtyamov@live.com>
2018-02-23 16:21:27 +0300

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	const float GRAVITY = 0.000000000066742f * 1000000000;
+	const float MAX_MASS = 10000.0f;
+	const float MIN_MASS = 100.0f;
+	const float MAX_FORCE = 15.0f;
+	const float MAX_VELOCITY = 12.0f;
+
+	public GameObject Graviton;
+	public GameObject Blackhole;
+	public GameObject Body;
+	public GameObject SpawPivot;
+
+	private List<GameObject> bodies = new List<GameObject>();
+	private Rigidbody2D gravitonRB;
+	private Rigidbody2D blackholeRB;
+	private CircleCollider2D blackholeCC;
+	private Transform spawnPivot;
+
+	void Start () {
+		gravitonRB = Graviton.GetComponent<Rigidbody2D>();
+		blackholeRB = Blackhole.GetComponent<Rigidbody2D>();
+		blackholeCC = Blackhole.GetComponent<CircleCollider2D>();
+		spawnPivot = SpawPivot.transform;
+	}
+
+	void Update() {
+		MoveCamera();
+		ProcessInput();
+		BlahBlah();
+		ApplyBodiesGravitation();
+		ApplyVelocityLimit();
+	}
+
+	void ProcessInput() {
+		if (Input.GetMouseButtonDown(0)) {
+			blackholeRB.mass = MIN_MASS;
+			Blackhole.GetComponent<SpriteRenderer>().enabled = true;
+			Debug.Log("DOWN " + blackholeRB.mass);
+		}
+		if (Input.GetMouseButton(0)) {
+			Blackhole.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+			ApplyGravitation(blackholeRB);
+			var massAcceleration = (MAX_MASS - MIN_MASS) * 0.1f;
+			blackholeRB.mass = Mathf.Min(blackholeRB.mass + massAcceleration * Time.deltaTime, MAX_MASS);
+			Debug.Log("HOLD " + blackholeRB.mass);
+			Debug.Log("VELO " + gravitonRB.velocity + "  " + gravitonRB.velocity.magnitude);
+		} else if (Input.GetMouseButtonUp(0)) {
+			blackholeRB.mass = MIN_MASS;
+			Blackhole.GetComponent<SpriteRenderer>().enabled = false;
+			Debug.Log("UP_0 " + blackholeRB.mass);
+		}
+		if (Input.GetMouseButtonUp(1)) {
+			blackholeRB.mass = MIN_MASS;
+			gravitonRB.AddForce(Vector2.zero, ForceMode2D.Force);
+			Debug.Log("UP_1 " + blackholeRB.mass);
+			gravitonRB.velocity = Vector2.zero;
+			Debug.Log("VELO " + gravitonRB.velocity + "  " + gravitonRB.velocity.magnitude);
+		}
+	}
+
+	void MoveCamera() {
+		var target = new Vector3(Graviton.transform.position.x, Graviton.transform.position.y, Camera.main.transform.position.z);
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, target, 1f);
+	}
+
+	void BlahBlah() {
+		var start = spawnPivot.position;
+		var end = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 1));
+	}
+
+	void SpawnBody(Vector3 position) {
+		var body = Object.Instantiate(Body);
+		body.GetComponent<Rigidbody2D>().mass = Random.Range(MIN_MASS, MAX_MASS);
+		body.transform.position = position;
+		bodies.Add(body);
+	}
+
+	void ApplyVelocityLimit() {
+		var multiplier = Mathf.Min(MAX_VELOCITY / gravitonRB.velocity.magnitude, 1);
+		gravitonRB.velocity = new Vector2(gravitonRB.velocity.x * multiplier, gravitonRB.velocity.y * multiplier);
+	}
+
+	void ApplyBodiesGravitation() {
+		for (var i = 0; i < bodies.Count; i++) {
+			ApplyGravitation(bodies[i].GetComponent<Rigidbody2D>());
+		}
+	}
+
+	void ApplyGravitation(Rigidbody2D body) {
+		var p0 = body.transform.position;
+		var p1 = gravitonRB.transform.position;
+		var m0 = body.mass;
+		var m1 = gravitonRB.mass;
+		var r = Vector3.Distance(p0, p1);
+		var force = m0 * m1 / (r * r) * GRAVITY * Time.deltaTime;
+		force = Mathf.Clamp(force, -MAX_FORCE, MAX_FORCE);
+		var vector = (p0 - p1).normalized * force;
+		gravitonRB.AddForce(vector, ForceMode2D.Force);
+	}
+}
---
+++ Assets/StarsParticleSystem.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class StarsParticleSystem : MonoBehaviour {
+
+	public GameObject Graviton;
+	public float Size;
+	public int Count;
+
+	private Rigidbody2D gravitonRB;
+	private ParticleSystem.Particle[] points;
+
+	// Use this for initialization
+	void Start() {
+		gravitonRB = Graviton.GetComponent<Rigidbody2D>();
+		MakeParticles();
+	}
+
+	private void MakeParticles() {
+		points = new ParticleSystem.Particle[Count];
+		for (int i = 0; i < Count; i++) {
+			points[i].position = getRandomPosition(false);
+			points[i].startColor = new Color(1, 1, 1, Random.value + 0.5f);
+			points[i].startSize = Size;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update() {
+		UpdateParticles();
+	}
+
+	void UpdateParticles() {
+		var velo = gravitonRB.velocity;
+		var va = velo.x < 0;
+		var vb = velo.y < 0;
+		var vc = velo.x > 0;
+		var vd = velo.y > 0;
+		for (int i = 0; i < Count; i++)
+		{
+			var position = Camera.main.WorldToViewportPoint(points[i].position);
+			var pa = position.x >= 1.33f;
+			var pb = position.y >= 1.33f;
+			var pc = position.x <= -0.33f;
+			var pd = position.y <= -0.33f;
+			var pe = position.z <= 0;
+			if (va && pa || vb && pb || vc && pc || vd && pd || pe)
+			{
+				points[i].position = getRandomPosition(true);
+			}
+		}
+		gameObject.GetComponent<ParticleSystem>().SetParticles(points, points.Length);
+	}
+
+	Vector3 getRandomPosition(bool isOutsideViewport) {
+		var velo = gravitonRB.velocity.normalized;
+		Vector3 point = new Vector3(
+			Random.Range(velo.x - 0.33f, 1.33f + velo.x),
+			Random.Range(velo.y - 0.33f, 1.33f + velo.y),
+			Mathf.Pow(Random.Range(0.05f, 0.99f), 0.33f) * Camera.main.farClipPlane
+		);
+		var px = point.y > 0 && point.y < 1;
+		var py = point.x > 0 && point.x < 1;
+		if (px && py && isOutsideViewport) return getRandomPosition(isOutsideViewport);
+		return Camera.main.ViewportToWorldPoint(point);
+	}
+}
